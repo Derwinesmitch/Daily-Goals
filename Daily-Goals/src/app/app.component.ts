8a04d422df2662c017aaa1733d9dfde13f41bde6
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-08 22:29:12 +0100

    Moved imports to App.module

--- Daily-Goals/src/app/app.component.ts
+++ Daily-Goals/src/app/app.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MainCardComponent } from './main-card/main-card.component';
-import { CommonModule, NgFor } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MainCardComponent, NgFor, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
