31749d11a88b08f19c58c10a27c15ab7f5874525
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-08 22:30:27 +0100

    Added Angular Fire Database for task loading

--- Daily-Goals/src/app/main-card/task-list/task-list.component.ts
+++ Daily-Goals/src/app/main-card/task-list/task-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TasklistitemComponent } from './tastklistitem/tastklistitem.component';
 import { CheckbuttonComponent } from '../../buttons/checkbutton/checkbutton.component';
-
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { NgFor } from '@angular/common';
 import { Task } from '../task.model';
 import { ButtonsComponent } from '../../buttons/buttons.component';
@@ -19,8 +19,16 @@ import { ButtonsComponent } from '../../buttons/buttons.component';
 })
 export class TaskListComponent {
   @Input() tasks: Task[] = [];
+
+  constructor(private db: AngularFireDatabase) {}
+
   ngOnInit() {
-    console.log('Tasks:', this.tasks);
+    this.db
+      .list<Task>('tasks')
+      .valueChanges()
+      .subscribe((tasks) => {
+        this.tasks = tasks;
+      });
   }
 
   deleteTask(task: Task) {
