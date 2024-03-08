9146739aae024bc811f089906abc7c26deaf5677
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-08 22:30:46 +0100

    Created enviroment for firebase config

---
+++ Daily-Goals/src/enviroments/enviroment.ts
@@ -0,0 +1,20 @@
+export const enviroment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyDtn61hTrwPDJXG1EJ6ST0znYBdkKqY7Q4',
+
+    authDomain: 'daily-goals-666f4.firebaseapp.com',
+
+    databaseURL:
+      'https://daily-goals-666f4-default-rtdb.europe-west1.firebasedatabase.app',
+
+    projectId: 'daily-goals-666f4',
+
+    storageBucket: 'daily-goals-666f4.appspot.com',
+
+    messagingSenderId: '49859302481',
+
+    appId: '1:49859302481:web:61194a7caf4be191d7d470',
+
+    measurementId: 'G-1QWKP9D0BM',
+  },
+};
