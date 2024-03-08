d55b95117fa774cdd94acd8f44124b6eabc09f67
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-03-08 22:28:39 +0100

    Added import for the AppModule

--- Daily-Goals/src/main.ts
+++ Daily-Goals/src/main.ts
@@ -1,6 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((success) => console.log(`Bootstrap success`))
   .catch((err) => console.error(err));
